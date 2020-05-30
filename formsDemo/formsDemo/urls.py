1f27f623710271239746c3392e62fcf13c74fc85
Kaushal Patel <beastanonymous007@gmail.com>
2020-05-30 09:57:37 +0530

    Forms Demo [DELETED]

--- formsDemo/formsDemo/urls.py
+++ formsDemo/formsDemo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from formsApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('userRegistration/', views.userRegistrationView, name='userRegistrationView')
 ]
