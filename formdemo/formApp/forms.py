51ea9b8befd7e6d457d8d5b75fcf8f1b6e0ded1b
Kaushal Patel <beastanonymous007@gmail.com>
2020-06-04 12:03:47 +0530

    Form Valadation [WORKING]

--- formdemo/formApp/forms.py
+++ formdemo/formApp/forms.py
@@ -8,3 +8,15 @@ class UserRegistrationForm(forms.Form):
     gender = forms.CharField(widget = forms.Select(choices=GENDER))
     password = forms.CharField(widget = forms.PasswordInput)
     ssn = forms.IntegerField()
+
+    def clean_firstName(self):
+        inputFirstName = self.cleaned_data['firstName']
+        if len(inputFirstName)>20:
+            raise forms.ValidationError('The max length of First Name is 20 Character')
+        return inputFirstName
+
+    def clean_email(self):
+        inpuEmail = self.cleaned_data['email']
+        if inpuEmail.find('@') == -1:
+            raise forms.ValidationError('The email should contain @')
+        return inpuEmail
