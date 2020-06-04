2a874e03bfc3c9740256b4dbf1727b1137df1f99
Kaushal Patel <beastanonymous007@gmail.com>
2020-06-04 11:56:01 +0530

    Forms Valadation [WORKING]

--- formdemo/formApp/forms.py
+++ formdemo/formApp/forms.py
@@ -2,8 +2,9 @@ from django import forms
 
 class UserRegistrationForm(forms.Form):
     GENDER = [('male','MALE'),('female','FEMALE')]
-    firstName = forms.CharField(required)
+    firstName = forms.CharField(required=False)
     lastName = forms.CharField()
     email = forms.CharField()
     gender = forms.CharField(widget = forms.Select(choices=GENDER))
     password = forms.CharField(widget = forms.PasswordInput)
+    ssn = forms.IntegerField()
